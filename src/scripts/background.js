8215d8ae7cc9c795e10ab68ca3f5b7f70caaddaf
satyam <satyampandey281@gmail.com>
2025-02-23 15:24:29 +0530

    button styling done

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,13 +1,16 @@
 console.log("Background script loaded!");
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("Received message:", message.message);
-  message_parameter = message.message;
-  if (message_parameter.action === "open_tab") {
-    chrome.tabs.create({ url: message_parameter.url }, (tab) => {
-      console.log(message_parameter.url);
-      console.log("New tab created:", tab);
-      sendResponse({ success: true });
-    });
-    return true; // Keeps the message channel open for async response
-  }
+chrome.runtime.onConnect.addListener((port) => {
+  console.log("connected to port ", port);
+  //   console.log("Received message:", message.message);
+  port.onMessage.addListener((message) => {
+    console.log("message contents: ",message)
+    if (message.action === "open_tab") {
+      chrome.tabs.create({ url: message.url }, (tab) => {
+        console.log(message.url);
+        console.log("New tab created:", tab);
+        sendResponse({ success: true });
+      });
+      return true; // Keeps the message channel open for async response
+    }
+  });
 });
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -12,14 +12,14 @@ class button {
 }
 
 window.addEventListener("load", () => {
-  for (let i = 1; i < 2; i++) {
-    console.log("youtube extensions loaded");
-  }
+  console.log("youtube extension loaded");
+
+  const port = chrome.runtime.connect(); // for messaging the background service worker
 
   let head = document.getElementsByTagName("head")[0];
   let video_title = head.getElementsByTagName("title")[0];
   const currentUrl = window.location.href;
-  console.log(currentUrl); // Logs the URL of the page where the content script runs
+  console.log("current page url :", currentUrl);
 
   const config = { attributes: true, childList: true, subtree: true };
 
@@ -27,43 +27,50 @@ window.addEventListener("load", () => {
     // console.log(ytInitialData);
     for (const mutation of mutationList) {
       if (mutation.type === "childList") {
-        console.log("A child node has been added or removed.");
+        console.log("youtube page changed/loaded");
         video_title_element = head.getElementsByTagName("title")[0];
 
         let video_title = video_title_element.textContent;
-        console.log("new title is :", video_title);
+        console.log("new video/page title is :", video_title);
 
         let playAll_exist = 0;
         if (document.getElementById("playAll_button") != undefined) {
           playAll_exist = 1;
         }
-        // const currentUrl = window.location.href;
 
         let newUrl = window.location.href;
-        console.log(newUrl); // Logs the URL of the page where the content script runs
+        console.log("page url after change: ", newUrl);
 
         if (newUrl.includes("watch")) {
-          console.log("Youtube video page");
+          //a youtube video page
+          console.log("Entered a Youtube video page");
           const yt_watch_metadata =
             document.getElementsByTagName("ytd-watch-metadata")[0];
-          console.log(yt_watch_metadata.querySelector("#top-row"));
+
+          console.log("youtube buttons area: ");
           const yt_buttons_row = yt_watch_metadata.querySelector("#top-row");
+          console.log(yt_buttons_row);
 
           if (!playAll_exist) {
             let channel_button = document.getElementsByClassName(
-              "yt-spec-button-shape-next yt-spec-button-shape-next--outline yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading"
+              "yt-spec-button-shape-next yt-spec-button-shape-next--outline yt-spec-button-shape-next--mono \
+               yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading"
             )[0];
-            console.log("before slicing channel id: ", channel_button.href);
-            let channelId = channel_button.href.replace(
-              "https://www.youtube.com/channel/",
-              ""
-            );
-            channelId = channelId.replace("/videos", "");
+            let channelId = channel_button.href
+              .replace("https://www.youtube.com/channel/", "")
+              .replace("/videos", "");
             console.log("channel id: ", channelId);
             let playAll_button = create_button("playAll", newUrl, channelId);
 
-            console.log("playlist link: ", playAll_button.playlist_link);
+            console.log(
+              "playAll playlist link: ",
+              playAll_button.playlist_link
+            );
+
             let playAll_html = document.createElement("button");
+            playAll_html.textContent = "Play all";
+            playAll_html.style.cssText =
+              "border-width:0px ; margin-right:8px; background: rgba(255,255,255,0.1) ; color: #f1f1f1;border-radius:20px; padding:0 14px; font-family: Roboto, Arial, sans-serif; font-weight: 500; font-size: 14px; ";
             const message = {
               action: "open_tab",
               url: playAll_button.playlist_link,
@@ -71,10 +78,16 @@ window.addEventListener("load", () => {
             console.log("Sending message:", message);
             playAll_html.addEventListener("click", (playAll_button) => {
               console.log("playAll button pressed");
-              chrome.runtime.sendMessage({ message });
+              port.postMessage(message);
+              // chrome.runtime.sendMessage({ message });
             });
             playAll_html.id = "playAll_button";
-            yt_buttons_row.appendChild(playAll_html);
+
+            const menu =
+              yt_buttons_row.getElementsByTagName("ytd-menu-renderer")[0];
+
+            menu.insertBefore(playAll_html, menu.children[0]);
+            // yt_buttons_row.appendChild(playAll_html);
             console.log(playAll_html);
           }
         }
